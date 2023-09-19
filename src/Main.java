b76e3a5b61ce0e0be106caaf5e63e64cefaab892
WalterMitty23 <23alexold23@gmail.com>
2023-09-19 04:24:26 -0300

    Билет 1 задача

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,10 @@ public class Main {
     public static void main(String[] args) {
 
         int[] array = new int[] {1, 2, 3};
-        reverseArray(array);
+        printArray(array);
     }
 
-    public static void reverseArray(int[] myArray) {
+    public static void printArray(int[] myArray) {
         for (int i = myArray.length - 1; i >= 0; i--) {
             System.out.print(myArray[i]);
             if (i > 0) {
